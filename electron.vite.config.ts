e62f7e586dc36511adeb7fc29ea7ada4995c183a
Yogesh Surendran <carreraphp@gmail.com>
2023-06-21 02:21:15 +0530

    chore(vite): add node ployfill for renderer

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import vpme from 'vite-plugin-monaco-editor'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 const { default: monacoEditorPlugin } = vpme
 
@@ -19,6 +20,18 @@ export default defineConfig({
       }
     },
     plugins: [
+      nodePolyfills({
+        // Specific modules that should not be polyfilled.
+        exclude: ['fs'],
+        // Whether to polyfill specific globals.
+        globals: {
+          Buffer: true, // can also be 'build', 'dev', or false
+          global: true,
+          process: true
+        },
+        // Whether to polyfill `node:` protocol imports.
+        protocolImports: true
+      }),
       react(),
       monacoEditorPlugin({
         languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript']
