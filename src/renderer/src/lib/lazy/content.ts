7a91951a2f3ff9e5085937fc9b6a266fcc48b4c0
Yogesh Surendran <carreraphp@gmail.com>
2023-09-25 11:43:58 +0530

    feat: add intl content for choose framework page

--- src/renderer/src/lib/lazy/content.ts
+++ src/renderer/src/lib/lazy/content.ts
@@ -1,8 +1,42 @@
 export const loadWelcomeContent = async (langCode: string): Promise<unknown> => {
-  let content
-
   const enContent = await import('../../assets/intl/en/welcome.json')
   const taContent = await import('../../assets/intl/ta/welcome.json')
+
+  return returnChoosenLanguageContent(langCode, taContent, enContent)
+}
+
+export const loadChooseFrameworkContent = async (langCode: string): Promise<unknown> => {
+  const enContent = await import('../../assets/intl/en/choose-framework.json')
+  const taContent = await import('../../assets/intl/ta/choose-framework.json')
+
+  return returnChoosenLanguageContent(langCode, taContent, enContent)
+}
+
+export const loadContent = async (pageName: string, langCode: string): Promise<unknown> => {
+  let content
+
+  switch (pageName) {
+    case 'welcome':
+      content = await loadWelcomeContent(langCode)
+      break
+
+    case 'choose-framework':
+      content = await loadChooseFrameworkContent(langCode)
+      break
+
+    default:
+      throw new Error('Selected Page is not available')
+      break
+  }
+
+  return content
+}
+function returnChoosenLanguageContent(
+  langCode: string,
+  taContent: unknown,
+  enContent: unknown
+): unknown {
+  let content
 
   switch (langCode) {
     case 'ta':
@@ -14,22 +48,5 @@ export const loadWelcomeContent = async (langCode: string): Promise<unknown> => 
       content = enContent
       break
   }
-
-  return content
-}
-
-export const loadContent = async (pageName: string, langCode: string): Promise<unknown> => {
-  let content
-
-  switch (pageName) {
-    case 'welcome':
-      content = await loadWelcomeContent(langCode)
-      break
-
-    default:
-      throw new Error('Selected Page is not available')
-      break
-  }
-
   return content
 }
